95f0f8bb6c382a9ae6597a97c57b4a1a289de96d
outlawleojung <outlawleojung@gmail.com>
2021-10-12 14:43:38 +0900

    add doc

--- windows/Hello_Client_win.c
+++ windows/Hello_Client_win.c
@@ -7,12 +7,14 @@ void ErrorHandling(char* message);
 int main(int argc, char* argv[]) {
 
 	WSADATA wsaData;
-	SOCKET hServerSock, hClntSock;
+	SOCKET hSocket;
 	SOCKADDR_IN servAddr, clntAddr;
 
 	int szClntAddr;
-	char message[] = "Hello World";
-	if (argc != 2) {
+	char message[30];
+	int strLen;
+
+	if (argc != 3) {
 		printf("Usage : %s <port>\n", argv[0]);
 		exit(1);
 	}
@@ -20,8 +22,8 @@ int main(int argc, char* argv[]) {
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		ErrorHandling("WSAStartup() error!");
 
-	hServerSock = socket(PF_INET, SOCK_STREAM, 0);
-	if (hServerSock == INVALID_SOCKET)
+	hSocket = socket(PF_INET, SOCK_STREAM, 0);
+	if (hSocket == INVALID_SOCKET)
 		ErrorHandling("socket() error");
 
 	memset(&servAddr, 0, sizeof(servAddr));
@@ -29,20 +31,15 @@ int main(int argc, char* argv[]) {
 	servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servAddr.sin_port = htonl(atoi(argv[1]));
 
-	if (bind(hServerSock, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
-		ErrorHandling("bind() error");
+	if (connect(hSocket, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+		ErrorHandling("connect() error");
 
-	if (listen(hServerSock, 5) == SOCKET_ERROR)
-		ErrorHandling("listen() error");
+	strLen = recv(hSocket, message, sizeof(message) - 1, 0);
+	if (strLen == -1)
+		ErrorHandling("read() error");
 
-	szClntAddr = sizeof(clntAddr);
-	hClntSock = accept(hServerSock, (SOCKADDR*)&clntAddr, &szClntAddr);
-	if (hClntSock == INVALID_SOCKET)
-		ErrorHandling("accept() error");
-
-	send(hClntSock, message, sizeof(message), 0);
-	closesocket(hClntSock);
-	closesocket(hServerSock);
+	printf("Message from server: %s \n", message);
+	closesocket(hSocket);
 	WSACleanup();
 
 	return 0;
